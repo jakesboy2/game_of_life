58053044659b8dd9e772f02392f14c763d53fa20
Jacob Waldrip <jwaldrip@okcthunder.com>
2019-02-20 02:34:37 -0600

    finished some more of the cell functions and created a main function

--- Cell.py
+++ Cell.py
@@ -1,22 +1,19 @@
 class Cell(object):
-    current_x = 0
-    current_y = 0
-    is_alive = False
-
-    next_x = 0
-    next_y = 0
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.is_alive = False
+        self.next_state = False
 
     # Get values for our next state
     def prepare_next_state(self):
-        pass
+        self.next_state = True
 
     # Enter cell into the next state using the stored values
     def enter_next_state(self):
-        pass
+        self.is_alive = self.next_state
+        self.next_state = False
 
     # Get a list of neighbors to the cell
     def get_all_neighbors(self):
@@ -27,12 +24,12 @@ class Cell(object):
         pass
 
     # Check if the cell is alvie
-    def is_alive(self):
+    def get_is_alive(self):
         return self.is_alive
 
     # return string repr of the object
     def __repr__(self):
-        if self.is_alive:
+        if self.is_alive is True:
             return "O"
         else:
             return "X"
--- main.py
+++ main.py
@@ -3,17 +3,54 @@ import Board
 import sys
 import time
 
-# Get the size of the board from the user or the command line
-if(len(sys.argv) > 1):
-    board_size = int(sys.argv[1])
-else:
-    board_size = int(input("Enter board size: "))
 
-# Set up the board and draw it on the screen
-board = Board.generate_new_board(board_size)
-Board.draw_board(board, board_size)
+# Loop over the board and get the next state for each cell
+def get_next_states(board, size):
+    for col in range(size):
+        for row in range(size):
+            board[col][row].prepare_next_state()
 
-while(True):
-    Board.clear_screen()
+
+# Loop over the board and apply for the next state for each cell
+def apply_next_states(board, size):
+    for col in range(size):
+        for row in range(size):
+            board[col][row].enter_next_state()
+
+
+def main():
+
+    # Coords are board[y][x]
+    # Get the size of the board from the user or the command line
+    if(len(sys.argv) == 2):
+        board_size = int(sys.argv[1])
+        generation_time = int(input("Enter generation speed: "))
+    elif(len(sys.argv) == 3):
+        print(sys.argv)
+        board_size = int(sys.argv[1])
+        generation_time = int(sys.argv[2])
+    else:
+        board_size = int(input("Enter board size: "))
+        generation_time = int(input("Enter generation speed: "))
+
+    # Set up the board and draw it on the screen
+    board = Board.generate_new_board(board_size)
     Board.draw_board(board, board_size)
-    time.sleep(1)
+
+    while(True):
+
+        # Clear screen of last generation
+        Board.clear_screen()
+
+        # Pre-update next state
+        get_next_states(board, board_size)
+        # Apply next state update
+        apply_next_states(board, board_size)
+
+        # Draw the new board and wait for next generation
+        Board.draw_board(board, board_size)
+        time.sleep(generation_time)
+
+
+if __name__ == '__main__':
+    main()
